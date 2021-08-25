91b9889a0309b55fd44cd1a1bf0355b38c8d73db
Yurii Rysyn <yurii.rysyn@yanpix.com>
2021-08-25 18:33:33 +0300

    Updates

--- precache-manifest.7840f428402e24d22fbfb274386c7a60.js
+++ precache-manifest.7840f428402e24d22fbfb274386c7a60.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c23794c9411a652d88597de40a177ff5",
+    "revision": "fb4fe17f564b5c56ebefa34fdbce4ade",
     "url": "/Sandbox/index.html"
   },
   {
-    "revision": "6b28238d9f3bc00d6cad",
+    "revision": "63d27be47b078042d707",
     "url": "/Sandbox/static/css/main.e8c1e5b5.chunk.css"
   },
   {
-    "revision": "37f17f121b0ccd9ff501",
-    "url": "/Sandbox/static/js/2.c2318676.chunk.js"
+    "revision": "7e3a1601e4a10fa22b9a",
+    "url": "/Sandbox/static/js/2.80f9b287.chunk.js"
   },
   {
     "revision": "570d362d673dab785e62d2b8563e1118",
-    "url": "/Sandbox/static/js/2.c2318676.chunk.js.LICENSE.txt"
+    "url": "/Sandbox/static/js/2.80f9b287.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "6b28238d9f3bc00d6cad",
-    "url": "/Sandbox/static/js/main.0abb78bb.chunk.js"
+    "revision": "63d27be47b078042d707",
+    "url": "/Sandbox/static/js/main.ecb358d7.chunk.js"
   },
   {
     "revision": "566dda306a53452aa190",
