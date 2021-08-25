695adf591f8171b20916a9a3e87abd34cc6e4f6a
Yurii Rysyn <yurii.rysyn@yanpix.com>
2021-08-25 13:49:04 +0300

    Updates

---
+++ precache-manifest.83c7b25fad10998acb7c3b4811408397.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab04946fab2a26d6181039335eb1a9cb",
+    "url": "/YuriiRysyn/Sandbox/index.html"
+  },
+  {
+    "revision": "aaf330fdec4f948752dc",
+    "url": "/YuriiRysyn/Sandbox/static/css/main.e8c1e5b5.chunk.css"
+  },
+  {
+    "revision": "05c630b7b64f97154c79",
+    "url": "/YuriiRysyn/Sandbox/static/js/2.060c677f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/YuriiRysyn/Sandbox/static/js/2.060c677f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aaf330fdec4f948752dc",
+    "url": "/YuriiRysyn/Sandbox/static/js/main.ac807891.chunk.js"
+  },
+  {
+    "revision": "6bace3eef48f8a31e8f9",
+    "url": "/YuriiRysyn/Sandbox/static/js/runtime-main.0a7998c9.js"
+  }
+]);
